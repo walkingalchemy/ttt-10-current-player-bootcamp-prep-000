1bdf82260a268250128449315e6dff83e53436c4
James Royer <walkingalchemy@gmail.com>
2017-10-10 03:59:01 +0000

    automatically backed up by learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,9 +9,6 @@ def turn_count(board)
 end
 
 def current_player(board)
-  if turn_count(board) % 2 == 0
-    return "X"
-  else
-    return "O"
-  end
+  turn_count(board) % 2 == 0 ?
+    "X" : "O"
 end
